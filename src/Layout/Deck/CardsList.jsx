d410e4787d71a3b6522e6a2c13195fc159f562a4
austinhjones3 <austin@austinjones.io>
2021-03-16 14:51:40 -0500

    added dir for img icons

--- src/Layout/Deck/CardsList.jsx
+++ src/Layout/Deck/CardsList.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function CardsList({ deck }) {
   return Object.keys(deck).length > 0
     ? deck.cards.map((card, index) => (
-        <div className="card">
+        <div className="card" key={index}>
           <div className="card-body">
             <span className="card-text d-flex justify-content-between">
               <p className="mr-5">{card.front}</p>
