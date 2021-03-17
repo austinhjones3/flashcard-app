dd86bc8f02303058840a9f2edd04f33db165c134
austinhjones3 <austin@austinjones.io>
2021-03-16 23:35:19 -0500

    create deck form skeleton built

--- src/Layout/Deck/View.jsx
+++ src/Layout/Deck/View.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { Fragment, useState, useEffect } from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import Study from "./Study";
--- src/Layout/Forms/AddDeck.jsx
+++ src/Layout/Forms/AddDeck.jsx
@@ -1,5 +1,35 @@
 import React, { Fragment } from "react";
 
 export default function AddDeck() {
-  return <Fragment>you do be adding a deck doe</Fragment>;
+  return (
+    <Fragment>
+      <h2>Create Deck</h2>
+      <form>
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Deck Name"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Description</label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            placeholder="Deck Description"
+          ></textarea>
+        </div>
+        <button type="button" className="btn btn-secondary mr-1" onClick="">
+          Cancel
+        </button>
+        <button type="submit" className="btn btn-primary" onClick="">
+          Submit
+        </button>
+      </form>
+    </Fragment>
+  );
 }
