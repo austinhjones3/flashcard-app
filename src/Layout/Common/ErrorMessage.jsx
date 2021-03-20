12e09864b733e965592e3959654d8c6494637a77
austinhjones3 <austin@austinjones.io>
2021-03-19 23:50:07 -0500

    error screen fixed

--- src/Layout/Common/ErrorMessage.jsx
+++ src/Layout/Common/ErrorMessage.jsx
@@ -1,13 +1,17 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+
 export default function ErrorMessage({ setError }) {
   setError(undefined);
   return (
-    <Fragment>
-      <h1>Bad things happened.</h1>
+    <div class="alert alert-danger" role="alert">
+      <h4 class="alert-heading">An error occurred during your last request.</h4>
+      <p>Please return to the home page and try again later.</p>
       <Link to="/">
-        <h3>Go Home</h3>
+        <h5 className="text-danger">
+          <span className="oi oi-home" /> Go Home
+        </h5>
       </Link>
-    </Fragment>
+    </div>
   );
 }
