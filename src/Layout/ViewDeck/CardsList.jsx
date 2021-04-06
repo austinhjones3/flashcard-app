8249b4912bf23da4f08c3bff4c747167d53c0372
austinhjones3 <austin@austinjones.io>
2021-04-05 19:02:28 -0500

    a few tweaks here and there

--- src/Layout/ViewDeck/CardsList.jsx
+++ src/Layout/ViewDeck/CardsList.jsx
@@ -30,33 +30,4 @@ export default function CardsList({ error, setError, setDeck, deck, url }) {
   } else {
     return null;
   }
-
-  // return Object.keys(deck).length > 0
-  //   ? deck.cards.map((card) => (
-  //       <div className="card mb-1" key={card.id}>
-  //         <div className="card-body">
-  //           <span className="card-text d-flex row">
-  //             <p className="col-5">{card.front}</p>
-  //             <p className="col-2"></p>
-  //             <p className="col-5">{card.back}</p>
-  //           </span>
-  //           <div className="float-right row">
-  //             <Link
-  //               className="btn btn-secondary"
-  //               to={`${url}/cards/${card.id}/edit`}
-  //             >
-  //               <span className="oi oi-pencil" /> Edit
-  //             </Link>
-  //             <CardDelete
-  //               error={error}
-  //               setError={setError}
-  //               deck={deck}
-  //               cardId={card.id}
-  //               setDeck={setDeck}
-  //             />
-  //           </div>
-  //         </div>
-  //       </div>
-  //     ))
-  //   : null;
 }
